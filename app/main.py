d8422800abce0fc1f8f403f0b026c4ae01c308e0
Mona-kecil <flowyourminds@gmail.com>
2024-12-22 11:17:42 +0700

    allow CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.parking_session_router import router as parking_session_router
 from app.routes.payment_router import router as payment_router
 
 app = FastAPI()
-# TODO: Allow CORS
+app.add_middleware(CORSMiddleware,
+                   allow_origins=['http://localhost:5173'],
+                   allow_credentials=True,
+                   allow_methods=['*'],
+                   allow_headers=['*'])
 # TODO: Implement Logging
 
 app.include_router(parking_session_router, prefix="/api/v1")
